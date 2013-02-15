6aca296b1c449b47558a5630153cc0285033fb80
Julien Quint <julien@igel.co.jp>
2013-02-15 14:29:44 +0900

    Keep momentum; stop

--- hb.js
+++ hb.js
@@ -1,121 +1,145 @@
-(function (hb) {
-  "use strict";
+"use strict";
 
-  var step_ms = 500;
-  var x = -3;
-  var y = -3;
-  var w = 7;
-  var h = 7;
-  var sz = 100;
-  var svg = document.querySelector("svg");
-  svg.setAttribute("viewBox", "%0 %1 %2 %3"
-    .fmt((x - 0.5) * sz, (y - 0.5) * sz, (w + 1) * sz, (h + 1) * sz));
+var ACTIONS = {
+  DOWN: function (program) {
+    program.cursor.vx = 0;
+    program.cursor.vy = 1;
+  },
+  LEFT: function (program) {
+    program.cursor.vx = -1;
+    program.cursor.vy = 0;
+  },
+  RIGHT: function (program) {
+    program.cursor.vx = 1;
+    program.cursor.vy = 0;
+  },
+  STOP: function (program) {
+    program.cursor.vx = 0;
+    program.cursor.vy = 0;
+  },
+  UP: function (program) {
+    program.cursor.vx = 0;
+    program.cursor.vy = -1;
+  },
+};
 
-  function icon(id) {
-    var use = flexo.$use({ "xlink:href": "#" + id });
-    flexo.make_property(use, "_x", function (x_) {
-      var x__ = flexo.clamp(x_, x, x + w - 1);
-      this.setAttribute("x", x__ * sz);
-      return x__;
-    });
-    flexo.make_property(use, "_y", function (y_) {
-      var y__ = flexo.clamp(y_, y, y + h - 1);
-      this.setAttribute("y", y__ * sz);
-      return y__;
-    });
-    use._x = 0;
-    use._y = 0;
-    return use;
+document.getElementById("STOP").setAttribute("points",
+    flexo.svg_polygon_points(8, 35, Math.PI / 8));
+
+var Cursor = {};
+
+Cursor.action = function () {
+  var mask = this.program.mask_at(this.x, this.y);
+  if (mask.icon) {
+    if (typeof mask.icon.action === "function") {
+      mask.icon.action(program);
+    } else {
+      program.push(mask.icon);
+    }
   }
+  mask.highlighted = false;
+  this.x = flexo.clamp(this.x + this.vx, this.program.x,
+      this.program.x + this.program.w - 1);
+  this.y = flexo.clamp(this.y + this.vy, this.program.y,
+      this.program.y + this.program.h - 1);
+  this.program.mask_at(this.x, this.y).highlighted = true;
+};
 
-  var grid = svg.appendChild(flexo.$g({ stroke: "black", fill: "none" }));
-  for (var i = 0; i <= w; ++i) {
-    grid.appendChild(flexo.$line({ x1: (x + i) * sz, x2: (x + i) * sz,
-      y1: y * sz, y2: (y + h) * sz }));
-  }
-  for (var j = 0; j <= h; ++j) {
-    grid.appendChild(flexo.$line({ y1: (y + j) * sz, y2: (y + j) * sz,
-      x1: x * sz, x2: (x + w) * sz }));
-  }
+var Program = {};
 
-  var list = Array.prototype.slice.call(document.querySelectorAll("defs > *"));
-  list.push("");
-  var icons = svg.appendChild(flexo.$g());
-  var cursor;
+Program.step = function () {
+  this.cursor.action();
+  window.setTimeout(this.step.bind(this), this.rate_ms);
+}
 
-  var masks = svg.appendChild(flexo.$g());
-  for (var i = 0; i < w; ++i) {
-    for (var j = 0; j < h; ++j) {
-      (function (i_, j_) {
-        var mask = masks.appendChild(flexo.$rect({ "fill-opacity": 0,
-          "stroke": "yellow", "stroke-linejoin": "round",
-          width: sz, height: sz,
-          x: (x + i_) * sz, y: (y + j_) * sz }));
-        flexo.make_readonly(mask, "_x", i_ + x);
-        flexo.make_readonly(mask, "_y", j_ + y);
-        flexo.make_property(mask, "highlighted", function (h) {
-          this.setAttribute("stroke-width", h ? 8 : 0);
-          return h;
-        });
-        flexo.make_property(mask, "icon", function (icon, prev) {
-          flexo.safe_remove(prev);
-          if (icon) {
-            icons.appendChild(icon);
-            icon._x = this._x;
-            icon._y = this._y;
-          }
-          return icon;
-        });
-        vs.addPointerListener(mask, vs.POINTER_END, function () {
-          var elem = flexo.random_element(list);
-          mask.icon = elem && icon(elem.getAttribute("id"));
-        }, false);
-        if (i_ === -x && j_ === -y) {
-          cursor = mask;
-          mask.highlighted = true;
-        } else {
-          mask.highlighted = false;
+Program.mask_at = function (x, y) {
+  return this.masks[y - this.y][x - this.x];
+}
+
+var program = (function () {
+  var program = Object.create(Program);
+  program.rate_ms = 350;
+  flexo.make_readonly(program, "x", -3);
+  flexo.make_readonly(program, "y", -3);
+  flexo.make_readonly(program, "w", 7);
+  flexo.make_readonly(program, "h", 7);
+  flexo.make_readonly(program, "cursor", (function () {
+    var cursor = Object.create(Cursor);
+    cursor.x = 0;
+    cursor.y = 0;
+    cursor.vx = 0;
+    cursor.vy = 0;
+    flexo.make_readonly(cursor, "program", program);
+    return cursor;
+  }()));
+  return program;
+}());
+
+var step_ms = 500;
+var sz = 100;
+var svg = document.querySelector("svg");
+svg.setAttribute("viewBox", "%0 %1 %2 %3".fmt((program.x - 0.5) * sz,
+    (program.y - 0.5) * sz, (program.w + 1) * sz, (program.h + 1) * sz));
+
+// Draw the grid
+var grid = document.getElementById("grid");
+for (var i = 0; i <= program.w; ++i) {
+  grid.appendChild(flexo.$line({ x1: (program.x + i) * sz,
+    x2: (program.x + i) * sz, y1: program.y * sz,
+    y2: (program.y + program.h) * sz }));
+}
+for (var j = 0; j <= program.h; ++j) {
+  grid.appendChild(flexo.$line({ y1: (program.y + j) * sz,
+    y2: (program.y + j) * sz, x1: program.x * sz,
+    x2: (program.x + program.w) * sz }));
+}
+
+// Icons layer
+var defs = Array.prototype.slice.call(document.querySelectorAll("defs > *"));
+var icons = document.getElementById("icons");
+
+// Draw the masks
+var masks = document.getElementById("masks");
+program.masks = [];
+for (var j = 0; j < program.h; ++j) {
+  program.masks.push([]);
+  for (var i = 0; i < program.w; ++i) {
+    (function (i_, j_) {
+      var mask = masks.appendChild(flexo.$rect({ width: sz, height: sz,
+        x: (program.x + i_) * sz, y: (program.y + j_) * sz }));
+      program.masks[j].push(mask);
+      flexo.make_readonly(mask, "_x", i_ + program.x);
+      flexo.make_readonly(mask, "_y", j_ + program.y);
+      flexo.make_property(mask, "highlighted", function (h) {
+        this.setAttribute("stroke-width", h ? 8 : 0);
+        return h;
+      });
+      flexo.make_property(mask, "counter", function (k, p) {
+        flexo.safe_remove(this.icon);
+        if (k > defs.length) {
+          k = 0;
+          delete this.icon;
         }
-      }(i, j));
-    }
+        if (k > 0) {
+          var id = defs[k - 1].id;
+          this.icon = icons.appendChild(flexo.$use({ "xlink:href": "#" + id,
+            x: (program.x + i_) * sz,
+            y: (program.y + j_) * sz }));
+          mask.icon.action = ACTIONS[id];
+        }
+        return k;
+      });
+      mask.counter = 0;
+      vs.addPointerListener(mask, vs.POINTER_END, function () {
+        ++mask.counter;
+      }, false);
+      mask.highlighted = false;
+    }(i, j));
   }
+}
 
-  var actions = {
-    "#primitive-up": function (cursor) {
-      move_cursor(0, -1);
-    },
-    "#primitive-right": function (cursor) {
-      move_cursor(1, 0);
-    },
-    "#primitive-down": function (cursor) {
-      move_cursor(0, 1);
-    },
-    "#primitive-left": function (cursor) {
-      move_cursor(-1, 0);
-    },
-  }
+document.addEventListener("touchstart", function (e) {
+  e.preventDefault();
+}, false);
 
-  function move_cursor(dx, dy) {
-    cursor.highlighted = false;
-    var x_ = flexo.clamp(cursor._x + dx, x, x + w - 1);
-    var y_ = flexo.clamp(cursor._y + dy, y, y + h - 1);
-    cursor = flexo.find_first(Array.prototype.slice.call(masks.childNodes),
-        function (m) {
-          return m._x === x_ && m._y === y_;
-        });
-    cursor.highlighted = true;
-    console.log(cursor.icon);
-  }
-
-  (function step() {
-    if (cursor.icon) {
-      actions[cursor.icon.getAttributeNS(flexo.ns.xlink, "href")]();
-    }
-    window.setTimeout(step, step_ms);
-  }());
-
-  document.addEventListener("touchstart", function (e) {
-    e.preventDefault();
-  }, false);
-
-}(this.hb = {}));
+program.step();
